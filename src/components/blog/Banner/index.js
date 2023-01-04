353dbf8028b7c39cfe30f01355c47b5a7bf97b3c
luangelsdorf <luanferreira2136@gmail.com>
2023-01-04 15:56:11 -0300

    Blog Banner

---
+++ src/components/blog/Banner/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'src/components/common/Button';
+import styles from './Banner.module.scss';
+
+export default function Banner({ content }) {
+  return (
+    <section className={styles.banner}>
+      <header>
+        <p className="overline large dark">Blog Pegada Neutra®</p>
+        <h1>Conhecimento que Transforma</h1>
+      </header>
+      <div className={styles.categories}>
+        <Button btnElement className="phthalo small">Todos</Button>
+        <Button btnElement className="phthalo small outline">Categoria 01</Button>
+        <Button btnElement className="phthalo small outline">Categoria 02</Button>
+        <Button btnElement className="phthalo small outline">Categoria 04</Button>
+      </div>
+    </section>
+  )
+}
