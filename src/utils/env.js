e82f7104180474f6b773e65c4c57606d15abf5ec
luangelsdorf <luanferreira2136@gmail.com>
2023-01-16 08:22:58 -0300

    Local API simulation

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,2 +1,3 @@
 export const apiURL = process.env.NEXT_PUBLIC_API_URL;
-export default process.env.NEXT_PUBLIC_ENV;
+export const env = process.env.NEXT_PUBLIC_ENV;
+export const dataSrc = process.env.DATA_SRC;
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,16 +1,26 @@
-export default async function fetchAPI(endpoint = '', queryString = '', populateDeep = true) {
-  const populate = populateDeep ? 'deep' : '*';
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/${endpoint}?populate=${populate}${queryString}`);
-  const result = await response.json();
+import { dataSrc } from "./env";
 
-  if (result.data.attributes) {
-    let { data: { attributes: data } } = result;
-    return data;
+export default async function fetchAPI(endpoint = '', queryString = '', populateDeep = true) {
+  if (dataSrc === 'local') {
+    /* console.log(`../data/${endpoint}.json`); */
+    return require(`../data/${endpoint}.json`);
+    /* console.log(data); */
   }
 
   else {
-    let { data: data } = result;
-    return data;
+    const populate = populateDeep ? 'deep' : '*';
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/${endpoint}?populate=${populate}${queryString}`);
+    const result = await response.json();
+
+    if (result.data.attributes) {
+      let { data: { attributes: data } } = result;
+      return data;
+    }
+
+    else {
+      let { data: data } = result;
+      return data;
+    }
   }
 }
 
