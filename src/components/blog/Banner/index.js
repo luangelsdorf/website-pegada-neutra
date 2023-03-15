5b4d66fcfc251afed56bd49f930fe5981e377054
luangelsdorf <luanferreira2136@gmail.com>
2023-03-15 15:37:11 -0300

    Banner Blog

--- src/components/blog/Banner/index.js
+++ src/components/blog/Banner/index.js
@@ -5,15 +5,17 @@ import styles from './Banner.module.scss';
 export default function Banner({ content }) {
   return (
     <div className={styles.banner}>
-      <header>
-        <p className="overline large dark">{content.title.overline}</p>
-        <h1>{content.title.title}</h1>
-      </header>
-      <div className={styles.categories}>
-        <Button btnElement className="phthalo small">Todos</Button>
-        <Button btnElement className="phthalo small outline">Categoria 01</Button>
-        <Button btnElement className="phthalo small outline">Categoria 02</Button>
-        <Button btnElement className="phthalo small outline">Categoria 04</Button>
+      <div className="col-12">
+        <header>
+          <p className="overline large dark">{content.title.overline}</p>
+          <h1>{content.title.title}</h1>
+        </header>
+        <div className={styles.categories}>
+          <Button btnElement className="phthalo small">Todos</Button>
+          <Button btnElement className="phthalo small outline">Categoria 01</Button>
+          <Button btnElement className="phthalo small outline">Categoria 02</Button>
+          <Button btnElement className="phthalo small outline">Categoria 04</Button>
+        </div>
       </div>
     </div>
   )
