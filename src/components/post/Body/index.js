a08e08c78cd0d5ce69d359d538a1c7e8292d87a4
luangelsdorf <luanferreira2136@gmail.com>
2023-04-11 15:49:51 -0300

    Post images fix

--- src/components/post/Body/index.js
+++ src/components/post/Body/index.js
@@ -3,9 +3,7 @@ import styles from './Body.module.scss';
 
 export default function Body({ content }) {
 
-  let postContent = content.replace('/uploads', `${process.env.NEXT_PUBLIC_API_URL}/uploads`);
-
   return (
-    <article className={styles.article} dangerouslySetInnerHTML={{ __html: postContent }} />
+    <article className={styles.article} dangerouslySetInnerHTML={{ __html: content }} />
   )
 }
