128c18712aad818d5048290c5a2985a3afe95594
luangelsdorf <luanferreira2136@gmail.com>
2023-05-15 08:16:44 -0300

    Post body typography fix

--- src/components/post/Body/index.js
+++ src/components/post/Body/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Body.module.scss';
 
 export default function Body({ content }) {
 
+  const newContent = content.replaceAll('<a', '<a class="greenery"')
+
   return (
-    <article className={styles.article} dangerouslySetInnerHTML={{ __html: content }} />
+    <article className={styles.article} dangerouslySetInnerHTML={{ __html: newContent }} />
   )
 }
