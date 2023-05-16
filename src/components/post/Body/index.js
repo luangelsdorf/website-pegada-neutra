74be5ffca2780a5f8b1ccc78e8f84ad622e4e231
luangelsdorf <luanferreira2136@gmail.com>
2023-05-16 11:09:55 -0300

    CKEditor image formatting

--- src/components/post/Body/index.js
+++ src/components/post/Body/index.js
@@ -1,11 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './Body.module.scss';
+import { env } from 'src/utils/env';
+import Head from 'next/head';
 
 export default function Body({ content }) {
 
-  const newContent = content.replaceAll('<a', '<a class="greenery"')
+  let newContent = content.replaceAll('<a', '<a class="greenery"');
+
+  if (env === 'dev') {
+    newContent = newContent.replaceAll('/uploads', `${process.env.NEXT_PUBLIC_API_URL}/uploads`);
+  }
 
   return (
-    <article className={styles.article} dangerouslySetInnerHTML={{ __html: newContent }} />
+    <>
+      <Head>
+        <link rel="stylesheet" href="/css/ckeditor.min.css" />
+      </Head>
+
+      <article className={`ck-content ${styles.article}`} dangerouslySetInnerHTML={{ __html: newContent }} />
+    </>
   )
 }
