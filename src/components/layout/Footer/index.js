2c167b1f1d966b33aaec6f8a3680a2d19309696e
luangelsdorf <luanferreira2136@gmail.com>
2023-05-17 16:16:38 -0300

    Small adjustments

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -81,10 +81,10 @@ export default function Footer({ content, light, info }) {
                     <Button className={`small ${light ? 'dark' : 'light'}`} link href="/sobre-nos#faq">Perguntas Frequentes</Button>
                   </li>
                   <li>
-                    <Button className={`small ${light ? 'dark' : 'light'}`} link href="#">Termos e Condições</Button>
+                    <Button className={`small ${light ? 'dark' : 'light'}`} link href="/termos">Termos e Condições</Button>
                   </li>
                   <li>
-                    <Button className={`small ${light ? 'dark' : 'light'}`} link href="#">Política de Privacidade</Button>
+                    <Button className={`small ${light ? 'dark' : 'light'}`} link href="/privacidade">Política de Privacidade</Button>
                   </li>
                 </ul>
                 <Button href={`https://wa.me/${clearNumber(info.phone)}`} target="_blank" className="outline phthalo small d-inline-flex d-lg-none" LeftIcon={TelephoneFill}>{info.phone}</Button>
